6c388fab31cdb1bc92313d89864d7d3bca07e759
Eertze van de Riet <eertze.0@gmail>
2025-09-07 18:12:53 +0200

    Added part 2 of 2019 #7

--- python/test/_2019/test_07_amplification_circuit.py
+++ python/test/_2019/test_07_amplification_circuit.py
@@ -84,7 +84,7 @@ def data(name: str):
         ),
     ],
 )
-def test_examples(nums, expected):
+def test_examples1(nums: list[int], expected: int):
     assert part1(nums) == expected
 
 
@@ -92,7 +92,20 @@ def test_input1():
     input = data("input")
     assert part1(input) == 366376
 
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5], 139629729),
+        ([3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10], 18216)
+    ],
+)
+def test_examples2(nums: list[int], expected: int):
+    assert part2(nums) == expected
+
 
 def test_input2():
     input = data("input")
-    assert part2(input) == ...
+    assert part2(input) == 21596786
