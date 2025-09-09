efe5afe39c29a48a2cfc967e9ea5ce3093961c6a
Eertze van de Riet <eertze.0@gmail>
2025-09-09 11:44:20 +0200

    Completed #21 2019

---
+++ python/src/_2019/day_21_springdroid_adventure.py
@@ -0,0 +1,58 @@
+import sys
+from functools import reduce
+
+from _2019.day_17_set_and_forget import asciify
+from _2019.intcode import IntCode
+from aoc.util import timed
+
+
+def parse(input: str) -> list[int]:
+    return [int(num) for num in input.split(",")]
+
+
+def jump_bot(memory: list[int], instructions: list[str]) -> int:
+    program = IntCode(memory)
+    inputs = reduce(
+        lambda acc, instruction: acc + asciify([instruction]), instructions, []
+    )
+    outputs = program.run(inputs)
+
+    return outputs[-1]
+
+
+def part1(memory: list[int]) -> int:
+    return jump_bot(
+        memory, ["NOT A J", "NOT B T", "OR T J", "NOT C T", "OR T J", "AND D J", "WALK"]
+    )
+
+
+def part2(memory: list[int]) -> int:
+    return jump_bot(
+        memory,
+        [
+            "OR A T",
+            "AND B T",
+            "AND C T",
+            "NOT T J",
+            "AND D J",
+            "OR H T",
+            "OR E T",
+            "AND T J",
+            "RUN",
+        ],
+    )
+
+
+def main():
+    input = sys.stdin.read()
+    parsed = parse(input)
+
+    result1, elapsed = timed(part1, parsed)
+    print(f"Part 1: {result1} ({elapsed} elapsed)")
+
+    result2, elapsed = timed(part2, parsed)
+    print(f"Part 2: {result2} ({elapsed} elapsed)")
+
+
+if __name__ == "__main__":
+    main()
