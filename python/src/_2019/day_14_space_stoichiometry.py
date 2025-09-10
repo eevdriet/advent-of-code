ac27559e48b19c194ae7e7f74d71f0f9d724d796
Eertze van de Riet <eertze.0@gmail>
2025-09-10 23:46:15 +0200

    Solved #14 of 2019

--- python/src/_2019/day_14_space_stoichiometry.py
+++ python/src/_2019/day_14_space_stoichiometry.py
@@ -1,18 +1,99 @@
 import sys
+from collections import defaultdict
+from math import ceil
 
+from attrs import define
+
+from aoc.io import read_file
 from aoc.util import timed
 
-
-def parse(input: str) -> str:
-    return input.strip()
+Reaction = tuple[int, list[tuple[str, int]]]
 
 
-def part1(input: str) -> int:
-    return 0
+@define
+class NanoFactory:
+    reactions: dict[str, Reaction]
+
+    @classmethod
+    def parse(cls, input: str) -> "NanoFactory":
+        reactions = {}
+
+        for line in input.splitlines():
+            inputs, output = line.split(" => ")
+            out_quantity, out_chem = output.split()
+
+            in_chems = []
+
+            for input in inputs.split(", "):
+                in_quantity, in_chem = input.split()
+                in_chems.append((in_chem, int(in_quantity)))
+
+            reactions[out_chem] = int(out_quantity), in_chems
+
+        return cls(reactions)
+
+    def is_terminal(self, chem: str) -> bool:
+        return len(self.reactions[chem]) == 1
+
+    def produce_from_ore(
+        self, chem: str, quantity: int, chemicals: dict[str, int]
+    ) -> int:
+        # Produce ORE directly
+        if chem == "ORE":
+            return quantity
+
+        out_quantity, inputs = self.reactions[chem]
+
+        # No (additional) ore has to be used based on available chemicals
+        if chemicals[chem] >= quantity:
+            chemicals[chem] -= quantity
+            return 0
+
+        # Use everything already available
+        quantity -= chemicals[chem]
+        chemicals[chem] = 0
+
+        # For the remainder, produce as much as still needed
+        n_batches = ceil(quantity / out_quantity)
+        ore_needed = 0
+
+        for in_chem, in_quantity in inputs:
+            ore_needed += self.produce_from_ore(
+                in_chem, in_quantity * n_batches, chemicals
+            )
+
+        chemicals[chem] += (n_batches * out_quantity) - quantity
+
+        return ore_needed
 
 
-def part2(input: str) -> int:
-    return 0
+def parse(input: str) -> NanoFactory:
+    return NanoFactory.parse(input)
+
+
+def part1(factory: NanoFactory) -> int:
+    return factory.produce_from_ore("FUEL", 1, defaultdict(int))
+
+
+def part2(factory: NanoFactory) -> int:
+    MAX_ORE_QUANTITY = 1_000_000_000_000
+    max_fuel = 1
+
+    # Use a binary search to find the maximum fuel amount to produce
+    left = 0
+    right = MAX_ORE_QUANTITY
+
+    while left <= right:
+        mid = (right - left) // 2 + left
+        ore_quantity = factory.produce_from_ore("FUEL", mid, defaultdict(int))
+
+        if ore_quantity <= MAX_ORE_QUANTITY:
+            max_fuel = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return max_fuel
 
 
 def main():
@@ -27,4 +108,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    input = read_file(2019, 14, "example1")
+    part1(parse(input))
--- python/test/_2019/test_14_space_stoichiometry.py
+++ python/test/_2019/test_14_space_stoichiometry.py
@@ -1,4 +1,5 @@
 import pytest
+
 from _2019.day_14_space_stoichiometry import parse, part1, part2
 from aoc.io import open_file
 
@@ -8,18 +9,39 @@ def data(name: str):
         return parse(file.read())
 
 
-@pytest.mark.parametrize("xyz, expected", [])
-def test_examples(xyz, expected):
-    assert part1(xyz) == expected
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (1, 31),
+        (2, 165),
+        (3, 13312),
+        (4, 180697),
+        (5, 2210736),
+    ],
+)
+def test_examples1(n: int, expected: int):
+    example = data(f"example{n}")
+    assert part1(example) == expected
 
 
 def test_input1():
-    input = data('input')
-    assert part1(input) == ...
+    input = data("input")
+    assert part1(input) == 346961
 
 
-@pytest.mark.skip(reason="Skip until solution to part 1 found through AOC")
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (3, 82892753),
+        (4, 5586022),
+        (5, 460664),
+    ],
+)
+def test_examples2(n: int, expected: int):
+    example = data(f"example{n}")
+    assert part2(example) == expected
+
+
 def test_input2():
-    input = data('input')
-    assert part2(input) == ...
-
+    input = data("input")
+    assert part2(input) == 4065790
